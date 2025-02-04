3fb368e174f940d205af06dd1794a8a70526d4ae
Draco04k <161503241+Draco04k@users.noreply.github.com>
2025-02-04 21:18:19 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,24 +1,36 @@
 package main
 
+// imports
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
+//starts http server
 func main() {
-	godotenv.Load()
-	BOT_TOKEN := os.Getenv("BOT_TOKEN")
-	sess, err := discordgo.New("Bot " + BOT_TOKEN)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		http.HandleFunc("/", getRoot)
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Excalibur is at render.com now.. 🚀\n")
+}
 	// Handlers
 	sess.AddHandler(onGuildCreate)
 	sess.AddHandler(LeaveEveryServer)
